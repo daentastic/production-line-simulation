0b0eff656c90abbc6905148235e1a754bccc1c1b
Brisch, Daniel (SE-A/35) <daniel.brisch@volkswagen.de>
2024-03-22 17:30:09 +0100

    initial push

---
+++ backend/prodlinesim/src/main/java/de/dabr/prodlinesim/ProdlinesimApplication.java
@@ -0,0 +1,12 @@
+package de.dabr.prodlinesim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdlinesimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdlinesimApplication.class, args);
+	}
+}
